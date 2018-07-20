47993b5091539365a3bf3dbb8452159a1db5c8ba
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 22:13:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+  def create
+    #when we get our auth back, we find_or_create a new user using their facebook uid, 
+    #then we set up our own object to mirror the stuff we wanted from facebook
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name=auth['info']['name']
+      u.email=auth['info']['email']
+      u.image=auth['info']['image']
+    end
+      
 end
